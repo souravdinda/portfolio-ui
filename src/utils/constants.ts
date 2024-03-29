66a8d3603a39bdc48880a994a01de836338878b0
SOURAV DINDA <63599752+souravdinda@users.noreply.github.com>
2024-03-30 00:03:01 +0530

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const serviceConstants = {
-    SERVICE_DOMAIN: process.env.REACT_APP_SERVICE_DOMAIN ?? '',
+    SERVICE_DOMAIN: process.env.REACT_APP_SERVICE_DOMAIN ?? 'https://backend.souravdinda.com/portfolio-api',
 };
 
 export const profileConstants = {
-    PROFILE_ID: process.env.REACT_APP_PROFILE_ID ?? '',
+    PROFILE_ID: process.env.REACT_APP_PROFILE_ID ?? '65fb04dcf835be9d0f1f3eea',
 };
